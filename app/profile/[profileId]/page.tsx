08289aea9fa8fc5275ebb3a4e13100712992d045
yadukrishnan <yadhu4507@gmail.com>
2024-07-29 12:03:34 +0530

    dynamic routing

---
+++ app/profile/[profileId]/page.tsx
@@ -0,0 +1,12 @@
+
+type paramsType ={params:{profileId:string}}
+import React from 'react'
+
+function  ProfileId({params}:paramsType) {
+    console.log(params)
+  return (
+    <div> ProfileId</div>
+  )
+}
+
+export default  ProfileId
